6393578f7dd6265847d5c92d6a41b05666bb229a
Julian Forero <julian.forero1990@gmail.com>
2018-07-14 16:50:45 -0500

    First commit

---
+++ src/main/java/com/asd/activos/ActivosApplication.java
@@ -0,0 +1,15 @@
+package com.asd.activos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ActivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivosApplication.class, args);
+	}
+}
