1c5366ddc5887acdc588722de20544c178bcf4c5
Julian Forero <julian.forero1990@gmail.com>
2018-07-14 17:06:58 -0500

    Quito comentario

--- src/main/java/com/asd/activos/ActivosApplication.java
+++ src/main/java/com/asd/activos/ActivosApplication.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class ActivosApplication {
-	// this is my second commit
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ActivosApplication.class, args);
 	}
